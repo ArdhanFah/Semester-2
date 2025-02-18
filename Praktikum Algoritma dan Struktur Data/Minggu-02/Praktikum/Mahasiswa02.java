32e8f349a1c956a51ae93fe99b0bfc71d4b8feba
Ardhanfah <ardhandikri04@gmail.com>
2025-02-18 11:00:51 +0700

    Praktikum 1

---
+++ Praktikum Algoritma dan Struktur Data/Minggu-02/Praktikum/Mahasiswa02.java
@@ -0,0 +1,34 @@
+class Mahasiswa{
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    void tampilInformasi(){
+        System.out.printf("NIM: %s %nNama: %s %nKelas: %s %nIPK: %.1f %n", nim,nama,kelas,ipk);
+    }
+
+    void ubahKelas(String kelasBaru){
+        this.kelas = kelasBaru;
+    }
+
+    void updateIpk(double ipkBaru){
+        this.ipk = ipkBaru;
+    }
+
+    String nilaiKerja(double ipk){
+        if(ipk < 0.0 || ipk > 4.0){
+            return "IPK tidak valid, Harus antara 0.0 dan 4.0";
+        }else{
+            if(this.ipk >= 3.5){
+                return "Kinerja Sangat Baik";
+            }else if(ipk >= 3.0){
+                return "Kinerja Baik";
+            }else if(ipk >= 2.0){
+                return "Kinerja Cukup";
+            }else{
+                return "Kinerja Kurang";
+            }
+        }
+    }
+}
